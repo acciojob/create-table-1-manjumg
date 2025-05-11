de01f77415f6f5484d227cd4247ec86fa1f1825a
Manjunath M Goni <manjugoni007@gmail.com>
2025-05-11 10:48:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-let rowCounter = 3; // Start from 3 since two rows already exist
-
 function insert_Row() {
-    const table = document.getElementById("sampleTable");
+  const table = document.getElementById("sampleTable");
 
-    const newRow = table.insertRow(0);
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
+  const newRow = table.insertRow(0);
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
 
-    const rowName = `Row ${rowCounter++}`;
-
-    cell1.innerHTML = `<div class="double-box">${rowName}</div>`;
-    cell2.innerHTML = `<div class="double-box">${rowName}</div>`;
+  cell1.innerHTML = '<div class="double-box">New Cell1</div>';
+  cell2.innerHTML = '<div class="double-box">New Cell2</div>';
 }
